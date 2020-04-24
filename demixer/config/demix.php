dc5734248ae853b2d1051e4e4de445ae9e750d9c
Vladimir Vesely <vesely@netsearch.cz>
2020-04-24 11:39:54 +0200

    Create demixer env parametrization

---
+++ demixer/config/demix.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return [
+    'cluster_client' => env('CLUSTER_CLIENT'),
+
+    'btc_user' => env('BTC_CORE_USERNAME'),
+    'btc_pass' => env('BTC_CORE_PASSWORD'),
+    'btc_host' => env('BTC_CORE_HOSTNAME'),
+    'btc_port' => env('BTC_CORE_PORT'),
+
+    'ltc_user' => env('LTC_CORE_USERNAME'),
+    'ltc_pass' => env('LTC_CORE_PASSWORD'),
+    'ltc_host' => env('LTC_CORE_HOSTNAME'),
+    'ltc_port' => env('LTC_CORE_PORT')
+];
