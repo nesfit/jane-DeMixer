769ace92135960c691904df6ec5733f2bb90f97a
Vladimir Vesely <veselyv@fit.vutbr.cz>
2020-04-27 12:48:00 +0200

    Introduce refresh script

--- demixer/refresh.sh
+++ demixer/refresh.sh
@@ -1,7 +1,9 @@
-
+php artisan key:generate
 php artisan view:clear
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 
 php artisan config:cache
+
+composer install --no-interaction --no-suggest --classmap-authoritative
